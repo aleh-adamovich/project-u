f4e4b187fbd0c31f628f6717d5d87ae850e0b3d4
Aleh Adamovich <aleh.adamovich.v@gmail.com>
2024-02-23 02:08:41 +0300

    feat: create sidebar component

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -8,7 +8,13 @@ export const AppRouter = () => {
       <Routes>
         {
           Object.values(routeConfig)
-            .map(({path, element}) => <Route key={path} path={path} element={element}/>)
+            .map(({path, element}) =>
+              <Route
+                key={path}
+                path={path}
+                element={<div className='page'>{element}</div>}
+              />
+            )
         }
       </Routes>
     </Suspense>

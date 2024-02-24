3934c47f6a796e8f9a5aeecd7f4b13e78c32768a
Aleh Adamovich <aleh.adamovich.v@gmail.com>
2024-02-24 19:32:15 +0300

    feat: set up jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,19 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only first arg', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional class', () => {
+    expect(classNames('someClass', ['class1', 'class2'])).toBe('someClass class1 class2');
+  });
+
+  test('with mods', () => {
+    expect(classNames(
+      'someClass',
+      ['class1'],
+      { hovered: true, scrollable: false, undefined },
+    )).toBe('someClass class1 hovered');
+  });
+});

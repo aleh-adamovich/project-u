6a31e444f727222482514cbca307c6938be6d904
Aleh Adamovich <aleh.adamovich.v@gmail.com>
2024-02-24 20:57:46 +0300

    feat: add loader component

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage: FC<NotFoundPageProps> = (props) => {
+  const { className } = props;
+
+  return (
+    <div className={classNames(cls.notFoundPage, [className])}>
+      Page Not Found
+    </div>
+  );
+};
